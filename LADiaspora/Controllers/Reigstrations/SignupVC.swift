e954afc3d825a9a7ca119a77147d07f0f8395e21
abdul diallo <rahimi.diallo96@gmail.com>
2020-06-09 11:42:25 -0400

    install firebase

--- LADiaspora/Controllers/Reigstrations/SignupVC.swift
+++ LADiaspora/Controllers/Reigstrations/SignupVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SignupVC: UIViewController {
+class SignupVC: UIViewController, UIPickerViewDelegate {
 
     @IBOutlet weak var profileImageButton: UIButton!
     @IBOutlet weak var fullnametf: UITextField!
@@ -17,14 +17,20 @@ class SignupVC: UIViewController {
     @IBOutlet weak var passwordtf: UITextField!
     @IBOutlet weak var signupButton: UIButton!
     
+    private let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+        imagePicker.delegate = self
         
     }
     
     @IBAction func profileImagePicked(_ sender: UIButton) {
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func signUpClicked(_ sender: UIButton) {
@@ -42,12 +48,36 @@ class SignupVC: UIViewController {
 extension SignupVC {
     
     func configureUI() {
+        // set profile image button
         profileImageButton.setImage(UIImage.init(named: "pickprofile"), for: .normal)
+        //
         Util().designThetextfield(withTextField: fullnametf)
         Util().designThetextfield(withTextField: usernametf)
         Util().designThetextfield(withTextField: emailtf)
         Util().designThetextfield(withTextField: passwordtf)
         Util().roundButton(withButton: signupButton)
+    }
+    
+    func roundProfileImageButton() {
+        profileImageButton.layer.cornerRadius = 64.0
+        profileImageButton.layer.masksToBounds = true
+        profileImageButton.imageView?.contentMode = .scaleAspectFit
+        profileImageButton.imageView?.clipsToBounds = true
+        profileImageButton.layer.borderWidth = 2
+//        profileImageButton.layer.borderColor = UIColor.
+    }
+    
+}
+
+//MARK: - image picker handler
+
+extension SignupVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        profileImageButton.setImage(image, for: .normal)
+        roundProfileImageButton()
+        dismiss(animated: true, completion: nil)
     }
     
 }
---
+++ Pods/Target Support Files/Pods-LADiaspora/Pods-LADiaspora-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LADiaspora : NSObject
+@end
+@implementation PodsDummy_Pods_LADiaspora
+@end
