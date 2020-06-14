c9345c08bab4f27405f9769bb4c580e387294922
abdul diallo <rahimi.diallo96@gmail.com>
2020-06-14 09:48:02 -0400

    download the current user username, full name and image url.

--- LADiaspora/API/UserService.swift
+++ LADiaspora/API/UserService.swift
@@ -6,14 +6,20 @@
 //  Copyright © 2020 Abdul Diallo. All rights reserved.
 //
 
-import Foundation
+import Firebase
 
 struct UserService {
     
     static let shared = UserService()
     
     func fetchUserFeed() {
-        print("user feed fetched")
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        Constants.References.db.child("users").child(uid).observeSingleEvent(of: .value) { (snapshot) in
+            guard let values = snapshot.value as? [String:Any] else { return }
+            guard let fullname = values["full name"] as? String,
+                let username = values["username"] as? String,
+                let profileURL = values["profileURL"] as? String else { return }
+        }
     }
     
 }
