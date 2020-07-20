f2b28bfdb898cc4cc8b0ac6554c2eee06d3a41d5
abdul diallo <rahimi.diallo96@gmail.com>
2020-07-20 15:12:25 -0400

    add imageview and button to the view

--- LADiaspora/Views/Profile/HeaderViewEdit.swift
+++ LADiaspora/Views/Profile/HeaderViewEdit.swift
@@ -12,6 +12,25 @@ class HeaderViewEdit : UIView {
     
     private let user : User
     
+    let profileIMG : UIImageView = {
+       let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        view.clipsToBounds = true
+        view.layer.borderWidth = 3
+        view.layer.borderColor = UIColor.white.cgColor
+        view.backgroundColor = .orange
+        return view
+    }()
+    
+    let cButton : UIButton = {
+        let view = UIButton()
+        view.setTitle("Change profile image", for: .normal)
+        view.addTarget(self, action: #selector(changeProfileTapped), for: .touchUpInside)
+        view.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        view.setTitleColor(.white, for: .normal)
+        return view
+    }()
+    
     //MARK: - init
     
     init(user: User) {
@@ -22,6 +41,21 @@ class HeaderViewEdit : UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: -  helpers
+    
+    func configUI() {
+        profileIMG.roundView()
+        profileIMG.frame.size.width = 100
+        profileIMG.frame.size.height = 100
+        addSubview(profileIMG)
+        addSubview(cButton)
+//        cButton.cen
+    }
+    
+    @objc func changeProfileTapped() {
+        print("clicked..")
     }
     
 }
